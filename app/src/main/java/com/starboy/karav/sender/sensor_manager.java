be9645cbdb4b3324270070a36cf1780c84255775
Karunyapas Dangruan <ioscarhunter@gmail.com>
2015-04-18 21:06:10 +0700

    Update sensor_manager.java

--- app/src/main/java/com/starboy/karav/sender/sensor_manager.java
+++ app/src/main/java/com/starboy/karav/sender/sensor_manager.java
@@ -15,16 +15,10 @@ import android.os.Vibrator;
 
 import java.util.List;
 
-
 public class MainActivity extends ActionBarActivity implements SensorEventListener {
 
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
-    private TextView tv;
-    private SeekBar xbar;
-    private VerticalSeekBar ybar;
-    private Vibrator v;
-    private boolean flag;
     private float x;
     private float y;
     private float z;
@@ -33,10 +27,6 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.textView);
-        xbar = (SeekBar) findViewById(R.id.xbar);
-        ybar = (VerticalSeekBar) findViewById(R.id.ybar);
-        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         flag = false;
         senSensorManager = (SensorManager) this.getSystemService((SENSOR_SERVICE));
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
@@ -51,19 +41,7 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
             x = sensorEvent.values[0];
             y = sensorEvent.values[1];
             z = sensorEvent.values[2];
-            xbar.setProgress(1000 + (int) (x * 100));
-            ybar.setProgress(1000 + (int) (y * 100));
-            if ((-0.5 < x) && (x < 0.5) && ((-0.5 < y) && (y < 0.5))) {
-                if(!flag)
-                v.vibrate(200);
-                flag = true;
-            }
-            else{
-                flag = false;
-            }
-            tv.setText("x = " + x + "\ny = " + y + "\nz = " + z);
         }
-
     }
 
     @Override
@@ -82,4 +60,5 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         super.onResume();
         senSensorManager.registerListener(this, senAccelerometer,  SensorManager.SENSOR_DELAY_FASTEST);
     }
+    
 }
