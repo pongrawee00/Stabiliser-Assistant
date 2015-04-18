3a890de582884b665d2b39789e69c16001d520ee
Karunyapas Dangruan <ioscarhunter@gmail.com>
2015-04-18 21:05:52 +0700

    Update and rename Sensor_procrss.java to sensor_procrss.java

---
+++ app/src/main/java/com/starboy/karav/sender/sensor_procrss.java
@@ -0,0 +1,12 @@
+package com.starboy.karav.sender;
+
+public class sensor_procrss(){
+  private float xbase;
+  private float ybase;
+  public void setbase(float x,float y){
+    xbase = x;
+    ybase = y;
+  }
+  public boolean process(float x,float y){
+    
+}
